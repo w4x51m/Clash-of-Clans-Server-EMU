aace91707fd5f9abe25d6e9521833204c8fe9685
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-04-01 13:56:47 +0300

    Create Gamefiles.cs

---
+++ GL-Server/Files/CSV_Reader/Gamefiles.cs
@@ -0,0 +1,49 @@
+﻿namespace GL.Servers.CoC.Files.CSV_Reader
+{
+    using System.Collections.Generic;
+
+    using GL.Servers.CoC.Files.CSV_Helpers;
+    using GL.Servers.CoC.Logic.Enums;
+    using GL.Servers.Files.CSV_Reader;
+
+    internal class Gamefiles
+    {
+        internal readonly Dictionary<int, DataTable> DataTables = new Dictionary<int, DataTable>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Gamefiles"/> class.
+        /// </summary>
+        internal Gamefiles()
+        {
+            this.DataTables = new Dictionary<int, DataTable>(CSV.Gamefiles.Count);
+        }
+
+        internal DataTable Get(int _Index)
+        {
+            return this.DataTables[_Index];
+        }
+
+        internal DataTable Get(Gamefile _Index)
+        {
+            return this.DataTables[(int) _Index];
+        }
+
+        internal Data GetWithGlobalID(int GlobalID)
+        {
+            int Type = 0;
+
+            while (GlobalID >= 1000000)
+            {
+                Type += 1;
+                GlobalID -= 1000000;
+            }
+
+            return this.DataTables[Type].GetDataWithInstanceID(GlobalID);
+        }
+
+        internal void Initialize(Table _Table, int _Index)
+        {
+            this.DataTables.Add(_Index, new DataTable(_Table, _Index));
+        }
+    }
+}
