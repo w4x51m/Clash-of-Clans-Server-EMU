885d51fe40f5dc0be3aaafd26d6633e4786ee3d8
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-04-01 14:12:46 +0300

    Add files via upload

---
+++ GL-Server/Files/NPC.cs
@@ -0,0 +1,30 @@
+﻿using GL.Servers.CoC.Files.CSV_Logic;
+using GL.Servers.CoC.Logic.Enums;
+
+namespace GL.Servers.CoC.Files
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    internal class NPC
+    {
+        /// <summary>
+        ///     A Dictionary of levels which can be loaded and used.
+        /// </summary>
+        internal static readonly Dictionary<int, string> Levels = new Dictionary<int, string>();
+
+        /// <summary>
+        ///     Initialize a new instance of the <see cref="NPC" /> class.
+        /// </summary>
+        internal NPC()
+        {
+            foreach (Npcs Npc in CSV.Tables.Get(Gamefile.Npcs).Datas)
+            {
+                NPC.Levels.Add(Npc.GlobalID, File.ReadAllText(@"Gamefiles/" + Npc.LevelFile));
+            }
+
+            Console.WriteLine(NPC.Levels.Count + " NPC Files, loaded and stored in memory.\n");
+        }
+    }
+}
