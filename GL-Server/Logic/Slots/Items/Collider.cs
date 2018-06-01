3f04b8c1f2e73fbf706a5b01fa63669f7a9f32b5
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 17:26:38 +0300

    Add files via upload

---
+++ GL-Server/Logic/Slots/Items/Collider.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace GL.Servers.CoC.Logic.Slots.Items
+{
+    internal class Collider
+    {
+        internal Objects Objects;
+        internal int[][] Map;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Collider"/>.
+        /// </summary>
+        /// <param name="Objects"></param>
+        internal Collider(Objects Objects)
+        {
+            this.Objects = Objects;
+            this.Map     = new int[50][];
+
+            for (int i = 0; i < 50; i++)
+            {
+                this.Map[i] = new int[50];
+            }
+        }
+
+        /// <summary>
+        /// Move object.
+        /// </summary>
+        /// <param name="oldX">Old object x position.</param>
+        /// <param name="oldY">Old object y position.</param>
+        /// <param name="x">New object x position.</param>
+        /// <param name="y">New object y position.</param>
+        /// <param name="width">Object width size.</param>
+        /// <param name="height">Object height size.</param>
+        /// <param name="ID">Object unique identifier.</param>
+        /// <returns></returns>
+        internal bool Move(int oldX, int oldY, int x, int y, int width, int height, int ID)
+        {
+            for (int x_pos = 0; x_pos < width; x_pos++)
+            {
+                for (int y_pos = 0; y_pos < height; y_pos++)
+                {
+                    if (this.Map[x + x_pos][y + y_pos] == 0 || this.Map[x + x_pos][y + y_pos] == ID)
+                    {
+                        this.Map[oldX + x_pos][oldY + y_pos]    = 0;
+                        this.Map[x + x_pos][y + y_pos]          = ID;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Collision with : " + this.Map[x + x_pos][y + y_pos]);
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Set object position without check collision.
+        /// </summary>
+        /// <param name="x">New object x position.</param>
+        /// <param name="y">New object y position.</param>
+        /// <param name="width">Object width size.</param>
+        /// <param name="height">Object height size.</param>
+        /// <param name="ID">Object unique identifier.</param>
+        internal void Set(int x, int y, int width, int height, int ID)
+        {
+            for (int x_pos = 0; x_pos < width; x_pos++)
+            {
+                for (int y_pos = 0; y_pos < height; y_pos++)
+                {
+                    this.Map[x + x_pos][y + y_pos] = ID;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Return if x and y position is free.
+        /// </summary>
+        /// <param name="x">New object x position.</param>
+        /// <param name="y">New object y position.</param>
+        /// <returns>is free.</returns>
+        internal bool isFree(int x, int y)
+        {
+            return this.Map[x][y] == 0;
+        }
+    }
+}
---
+++ GL-Server/Logic/Slots/Items/Deco.cs
@@ -0,0 +1,81 @@
+﻿namespace GL.Servers.CoC.Logic.Slots.Items
+{
+    using GL.Servers.CoC.Logic.Interfaces;
+
+    using Newtonsoft.Json;
+
+    internal class Deco : IGameObject
+    {
+        [JsonProperty("objects")]  internal Objects Objects;
+        [JsonIgnore]               internal Files.CSV_Logic.Decos Csv;
+
+        [JsonProperty("id")]       public int ID { get; set; }
+        [JsonProperty("data")]     public int Data { get; set; }
+
+        [JsonProperty("x")]        public int X { get; set; }
+        [JsonProperty("y")]        public int Y { get; set; }
+
+        [JsonProperty("l1x")]      public int? L1X { get; set; }
+        [JsonProperty("l1y")]      public int? L1Y { get; set; }
+        [JsonProperty("l2x")]      public int? L2X { get; set; }
+        [JsonProperty("l2y")]      public int? L2Y { get; set; }
+        [JsonProperty("l3x")]      public int? L3X { get; set; }
+        [JsonProperty("l3y")]      public int? L3Y { get; set; }
+        [JsonProperty("l4x")]      public int? L4X { get; set; }
+        [JsonProperty("l4y")]      public int? L4Y { get; set; }
+        [JsonProperty("l5x")]      public int? L5X { get; set; }
+        [JsonProperty("l5y")]      public int? L5Y { get; set; }
+        [JsonProperty("l6x")]      public int? L6X { get; set; }
+        [JsonProperty("l6y")]      public int? L6Y { get; set; }
+        [JsonProperty("l7x")]      public int? L7X { get; set; }
+        [JsonProperty("l7y")]      public int? L7Y { get; set; }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Deco"/>
+        /// </summary>
+        internal Deco()
+        {
+            // Deco.
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Deco"/>
+        /// </summary>
+        /// <param name="ID">The identifier.</param>
+        /// <param name="Data">The data identifier.</param>
+        internal Deco(int ID, int Data)
+        {
+            this.ID   = ID;
+            this.Data = Data;
+        }
+
+        /// <summary>
+        /// Refreshes the <see cref="Deco"/>.
+        /// </summary>
+        /// <param name="Tick">The time.</param>
+        public void Refresh(float Tick)
+        {
+            // Refresh
+        }
+
+        /// <summary>
+        /// Move the <see cref="Deco"/>.
+        /// </summary>
+        /// <param name="X">X position</param>
+        /// <param name="Y">Y position</param>
+        /// <returns>True if sucess</returns>
+        internal bool Move(int X, int Y)
+        {
+            if (this.Objects.Collider.Move(this.X, this.Y, X, Y, this.Csv.Width, this.Csv.Height, this.ID))
+            {
+                this.X = X;
+                this.Y = Y;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
