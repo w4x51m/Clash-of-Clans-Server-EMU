a88c62cef8c6500aac6c367064b77d9a5d3dbd13
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 16:43:12 +0300

    Create Achievements.cs

---
+++ GL-Server/Logic/Slots/Achievements.cs
@@ -0,0 +1,38 @@
+﻿namespace GL.Servers.CoC.Logic.Slots
+{
+    using System.Collections.Generic;
+
+    internal class Achievements : List<int>
+    {
+        internal Player Player;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Achievements"/>.
+        /// </summary>
+        internal Achievements()
+        {
+            // Achievements.
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Achievements"/>.
+        /// </summary>
+        /// <param name="Player">The player.</param>
+        internal Achievements(Player Player)
+        {
+            this.Player = Player;
+        }
+
+        internal bool Add(int ID)
+        {
+            if (!this.Contains(ID))
+            {
+                base.Add(ID);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
