137a11be6257b9591b1b890e30dafb0ac598f38c
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 13:13:53 +0300

    Add files via upload

---
+++ GL-Server/Files/GlobalsDefinition.cs
@@ -0,0 +1,26 @@
+﻿namespace GL.Servers.CoC.Files
+{
+    using System.IO;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    internal class Globals_definition
+    {
+        internal static string Json = string.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Home"/> class.
+        /// </summary>
+        internal Globals_definition()
+        {
+            if (Directory.Exists("Gamefiles/"))
+            {
+                if (File.Exists("Gamefiles/globals_definition.json"))
+                {
+                    Globals_definition.Json = File.ReadAllText("Gamefiles/globals_definition.json", Encoding.UTF8);
+                    Globals_definition.Json = Regex.Replace(Globals_definition.Json, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
+                }
+            }
+        }
+    }
+}
