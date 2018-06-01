26104a01888b1d29204b95ec3fb0ade94ac8eb10
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 16:35:19 +0300

    Add files via upload

---
+++ GL-Server/Logic/Enums/Variable.cs
@@ -0,0 +1,29 @@
+namespace GL.Servers.CoC.Logic.Enums
+{
+    public enum Variable
+    {
+        StarBonusCounter,
+        StarBonusCooldown,
+        StarBonusTimerEndSubTick,
+        StarBonusTimerEndTimestep,
+        StarBonusTimesCollected,
+        ChallengeStarted,
+        ChallengeLayoutIsWar,
+        FriendListLastOpened,
+        BeenInArrangedWar, 
+        FILL_ME,
+        AccountBound, 
+        EventUseTroop,
+        VillageToGoTo,
+        LootLimitWinCount,
+        LootLimitTimerEndSubTick,
+        LootLimitTimerEndTimestamp,
+        LootLimitCooldown,
+        Village2BarrackLevel,
+        LootLimitFreeSpeedUp,
+        SeenBuilderMenu,
+        MaxArmyTimerEndSubTick,
+        MaxArmyTimerEndTimeStamp,
+        MaxArmyTimerPausedTicksLeft,
+    }
+}
