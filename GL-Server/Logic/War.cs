f57e0a04087490ad5d5995e36db4e9bd31bac650
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 13:25:38 +0300

    Create War.cs

---
+++ GL-Server/Logic/War.cs
@@ -0,0 +1,35 @@
+﻿using ﻿GL.Servers.CoC.Logic;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+﻿namespace GL.Servers.CoC.Logic
+{
+    public class War : BadRequest
+    {
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("teamSize")]
+        public int TeamSize { get; set; }
+
+        [JsonProperty("preparationStartTime"), JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime PreparationStartTime { get; set; }
+
+        [JsonProperty("startTime"), JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime StartTime { get; set; }
+
+        [JsonProperty("endTime"), JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime EndTime { get; set; }
+
+        [JsonProperty("clan")]
+        public WarClan Clan { get; set; }
+
+        [JsonProperty("opponent")]
+        public WarClan Opponent { get; set; }
+
+    }
+}
