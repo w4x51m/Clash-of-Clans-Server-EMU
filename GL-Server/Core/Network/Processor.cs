d3548e1af1c898823f9efbc7c46b8b2fe9e57e9e
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 20:50:12 +0300

    Create Processor.cs

---
+++ GL-Server/Core/Network/Processor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace GL.Servers.CoC.Core.Network
+{
+    using GL.Servers.CoC.Packets;
+
+    internal static class Processor
+    {
+        /// <summary>
+        /// Recepts the specified message.
+        /// </summary>
+        /// <param name="Message">The message.</param>
+        internal static void Recept(this Message Message)
+        {
+            Message.Decrypt();
+            Message.Decode();
+            Message.Process();
+        }
+
+        /// <summary>
+        /// Sends the specified message.
+        /// </summary>
+        /// <param name="Message">The message.</param>
+        internal static void Send(this Message Message)
+        {
+            Message.Encode();
+            Message.Encrypt();
+
+            Logging.Error(typeof(Processor), "Packet " + Message.Identifier + " sent to " + Message.Device.Socket.RemoteEndPoint + ".");
+
+            Resources.Gateway.Send(Message);
+
+            Message.Process();
+        }
+
+        /// <summary>
+        /// Handles the specified command.
+        /// </summary>
+        /// <param name="Command">The command.</param>
+        internal static Command Handle(this Command Command)
+        {
+            Command.Encode();
+
+            return Command;
+        }
+    }
+}
