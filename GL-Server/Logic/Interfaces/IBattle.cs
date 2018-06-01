2a1b9fc5e59834842632afb23402bca879325a5d
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 16:40:07 +0300

    Create IBattle.cs

---
+++ GL-Server/Logic/Interfaces/IBattle.cs
@@ -0,0 +1,22 @@
+﻿namespace GL.Servers.CoC.Logic.Interfaces
+{
+    using GL.Servers.CoC.Packets;
+
+    using System.Collections.Generic;
+
+    public interface IBattle
+    {
+        Objects Level { get; set; }
+
+        Player Attacker { get; set; }
+        Player Defender { get; set; }
+
+        int EndTick { get; set; }
+        int Timestamp { get; set; }
+
+        List<Command> Commands { get; set; }
+
+        void Refresh(float Tick);
+        void Process(Command Comamnd);
+    }
+}
