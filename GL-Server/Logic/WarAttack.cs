406a6e7a0a66ecf43ea936d4d488cb2eb92f6a92
Clash_LAND <37537996+Enjoyop@users.noreply.github.com>
2018-06-01 13:27:19 +0300

    Create WarAttack.cs

---
+++ GL-Server/Logic/WarAttack.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+﻿namespace GL.Servers.CoC.Logic
+{
+    public class WarAttack
+    {
+        [JsonProperty("attackerTag")]
+        public string AttackerTag { get; set; }
+
+        [JsonProperty("defenderTag")]
+        public string DefenderTag { get; set; }
+
+        [JsonProperty("stars")]
+        public int Stars { get; set; }
+
+        [JsonProperty("destructionPercentage")]
+        public decimal DestructionPercentage { get; set; }
+
+        [JsonProperty("order")]
+        public int Order { get; set; }
+
+    }
+}
