803168513419ebad261c14b0cafc5bec5a75c1f9
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-02 09:28:18 +0300

    Create FloatConverter.cs

---
+++ GL-Server/Extensions/Json/FloatConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace GL.Servers.CoC.Extensions.Json
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    internal class FloatConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteRawValue(JsonConvert.SerializeObject((int)(float)value));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public override bool CanWrite { get; } = true;
+        public override bool CanRead { get; } = false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(float) || objectType == typeof(float?);
+        }
+    }
+}
