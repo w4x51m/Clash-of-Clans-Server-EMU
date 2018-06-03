d0e770bf159667f007779d81c1fb65d1b680c3e9
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:40:39 +0300

    Create GemsAdded.cs

---
+++ GL-Server/Packets/Commands/Server/GemsAdded.cs
@@ -0,0 +1,36 @@
+﻿using GL.Servers.Extensions.List;
+
+namespace GL.Servers.CoC.Packets.Commands.Server
+{
+    using GL.Servers.CoC.Logic;
+
+    internal class GemsAdded : Command
+    {
+        private int Gems;
+        private int Package;
+        private int OfferID;
+
+        private bool Gift;
+
+        public GemsAdded(Device Device, int Gems, int Package = -1, int OfferID = -1, bool Gift = false) : base(Device)
+        {
+            this.Identifier = 7;
+
+            this.Gems       = Gems;
+            this.Package    = Package;
+            this.OfferID    = OfferID;
+
+            this.Gift       = Gift;
+        }
+
+        internal override void Encode()
+        {
+            this.Data.AddBool(this.Gift);
+            this.Data.AddInt(this.Gems);
+            this.Data.AddInt(this.Package);
+            this.Data.AddInt(this.OfferID);
+            this.Data.AddInt(this.Gift ? 1 : 0);
+            this.Data.AddString(null);
+        }
+    }
+}
