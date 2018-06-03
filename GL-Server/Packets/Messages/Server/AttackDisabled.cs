ab6489223abb7915fbfe6eda94dfa8cf580c1e32
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 18:29:23 +0300

    Create AttackDisabled.cs

---
+++ GL-Server/Packets/Messages/Server/AttackDisabled.cs
@@ -0,0 +1,24 @@
+﻿namespace GL.Servers.CoC.Packets.Messages.Server
+{
+    using GL.Servers.CoC.Extensions.List;
+    using GL.Servers.CoC.Logic;
+
+    internal class AttackDisabled : Message
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttackDisabled"/> class.
+        /// </summary>
+        /// <param name="Device">The device.</param>
+        public AttackDisabled(Device Device) : base(Device)
+        {
+            this.Identifier = 24121;
+        }
+
+        /// <summary>
+        /// Encodes the <see cref="Message" />, using the <see cref="Writer" /> instance.
+        /// </summary>
+        internal override void Encode()
+        {
+        }
+    }
+}
