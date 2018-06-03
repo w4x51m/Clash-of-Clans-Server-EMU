0f85500ca9c0044518ffc9f94cd6c13d533ce368
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:45:19 +0300

    Create AskClanData.cs

---
+++ GL-Server/Packets/Messages/Client/AskClanData.cs
@@ -0,0 +1,48 @@
+﻿using GL.Servers.CoC.Core;
+using GL.Servers.CoC.Core.Network;
+using GL.Servers.CoC.Packets.Messages.Server;
+
+namespace GL.Servers.CoC.Packets.Messages.Client
+{
+    using GL.Servers.CoC.Logic;
+
+    using GL.Servers.Extensions.Binary;
+
+    internal class AskClanData : Message
+    {
+        private int HighID;
+        private int LowID;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AskClanData"/> class.
+        /// </summary>
+        /// <param name="Device">The device.</param>
+        /// <param name="Reader">The reader.</param>
+        public AskClanData(Device Device, Reader Reader) : base(Device, Reader)
+        {
+            // AskClanData.
+        }
+
+        /// <summary>
+        /// Decodes this message.
+        /// </summary>
+        internal override void Decode()
+        {
+            this.HighID = this.Reader.ReadInt32();
+            this.LowID  = this.Reader.ReadInt32();
+        }
+
+        /// <summary>
+        /// Processes this message.
+        /// </summary>
+        internal override void Process()
+        {
+            Clan Clan = Resources.Clans.Get(this.HighID, this.LowID, Store: false);
+
+            if (Clan != null)
+            {
+                new ClanData(this.Device, Clan).Send();
+            }
+        }
+    }
+}
