8ec3d12847e45ab885b234d85c97eb0e1dac3b6f
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 11:44:40 +0300

    Create ChangeVillage.cs

---
+++ GL-Server/Packets/Commands/Client/ChangeVillage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GL.Servers.CoC.Packets.Commands.Client
+{
+    using GL.Servers.CoC.Logic;
+    using GL.Servers.Extensions.Binary;
+
+    internal class ChangeVillage : Command
+    {
+        internal int Tick;
+
+        public ChangeVillage(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            
+        }
+
+        internal override void Process()
+        {
+            
+        }
+    }
+}
