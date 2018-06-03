d36ce030c5179570f5d1135c5bc93e530a7aad83
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:37:14 +0300

    Create ChangeName.cs

---
+++ GL-Server/Packets/Commands/Server/ChangeName.cs
@@ -0,0 +1,32 @@
+﻿using GL.Servers.Extensions.List;
+
+namespace GL.Servers.CoC.Packets.Commands.Server
+{
+    using GL.Servers.CoC.Logic;
+    using GL.Servers.Extensions.Binary;
+
+    internal class ChangeName : Command
+    {
+        private string Name;
+        private int State;
+
+        public ChangeName(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+
+        }
+
+        public ChangeName(Device Device, string Name, int State) : base(Device)
+        {
+            this.Identifier = 3;
+            this.Name       = Name;
+            this.State      = State;
+        }
+
+        internal override void Encode()
+        {
+            this.Data.AddString(this.Name);
+            this.Data.AddInt(this.State);
+            this.Data.AddInt(0);
+        }
+    }
+}
