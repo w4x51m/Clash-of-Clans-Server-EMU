ebf0e5003851b7b32fee8ecd5d15825cf9b6ed16
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:17:18 +0300

    Add files via upload

---
+++ GL-Server/Packets/Commands/Client/Matchmaking.cs
@@ -0,0 +1,28 @@
+﻿using GL.Servers.CoC.Core;
+
+namespace GL.Servers.CoC.Packets.Commands.Client
+{
+    using GL.Servers.CoC.Logic;
+    using GL.Servers.Extensions.Binary;
+
+    internal class Matchmaking : Command
+    {
+        public Matchmaking(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            this.Reader.ReadInt32();
+            this.Reader.ReadInt32();
+
+            this.Tick = this.Reader.ReadInt32();
+        }
+
+        internal override void Process()
+        {
+            Resources.Battles.Start_PVP(this.Device.Player);
+        }
+    }
+}
---
+++ GL-Server/Packets/Commands/Client/MissionProgress.cs
@@ -0,0 +1,30 @@
+﻿namespace GL.Servers.CoC.Packets.Commands.Client
+{
+    using GL.Servers.CoC.Logic;
+    using GL.Servers.Extensions.Binary;
+
+    using GL.Servers.CoC.Files;
+    using GL.Servers.CoC.Files.CSV_Logic;
+    using GL.Servers.CoC.Logic.Enums;
+
+    internal class MissionProgress : Command
+    {
+        private int ID;
+
+        public MissionProgress(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            this.ID   = this.Reader.ReadInt32();
+            this.Tick = this.Reader.ReadInt32();
+        }
+
+        internal override void Process()
+        {
+            this.Device.Player.Missions.Add(this.ID, CSV.Tables.Get(Gamefile.Missions).GetDataWithID(this.ID) as Missions);
+        }
+    }
+}
