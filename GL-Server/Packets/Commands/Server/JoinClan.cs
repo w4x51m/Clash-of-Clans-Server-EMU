2196aa1f620479e41d2740bcc57839100149ec0e
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:41:33 +0300

    Create JoinClan.cs

---
+++ GL-Server/Packets/Commands/Server/JoinClan.cs
@@ -0,0 +1,17 @@
+﻿namespace GL.Servers.CoC.Packets.Commands.Server
+{
+    using GL.Servers.CoC.Logic;
+
+    internal class JoinClan : Command
+    {
+        public JoinClan(Device Device) : base(Device)
+        {
+            this.Identifier = 1;
+        }
+
+        internal override void Encode()
+        {
+
+        }
+    }
+}
