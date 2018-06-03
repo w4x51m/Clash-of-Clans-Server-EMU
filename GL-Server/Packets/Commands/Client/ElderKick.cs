6c2a2b6b7f904cea48b2a001d6243b95237848b5
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 12:41:54 +0300

    Create ElderKick.cs

---
+++ GL-Server/Packets/Commands/Client/ElderKick.cs
@@ -0,0 +1,23 @@
+﻿namespace GL.Servers.CoC.Packets.Commands.Client
+{
+    using GL.Servers.CoC.Logic;
+    using GL.Servers.Extensions.Binary;
+
+    internal class ElderKick : Command
+    {
+        public ElderKick(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            this.Debug();
+        }
+
+        internal override void Process()
+        {
+            base.Process();
+        }
+    }
+}
