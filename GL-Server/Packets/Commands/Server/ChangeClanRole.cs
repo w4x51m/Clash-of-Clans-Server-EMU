adb3c22244cdb9be0373b848cf21d76a1286749c
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:33:29 +0300

    Create ChangeClanRole.cs

---
+++ GL-Server/Packets/Commands/Server/ChangeClanRole.cs
@@ -0,0 +1,17 @@
+﻿namespace GL.Servers.CoC.Packets.Commands.Server
+{
+    using GL.Servers.CoC.Logic;
+
+    internal class ChangeClanRole : Command
+    {
+        public ChangeClanRole(Device Device) : base(Device)
+        {
+            this.Identifier = 8;
+        }
+
+        internal override void Encode()
+        {
+
+        }
+    }
+}
