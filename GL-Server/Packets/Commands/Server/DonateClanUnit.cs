20f2138fa34a0950d01610eb98b4c5d394e8e14c
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:39:52 +0300

    Create DonateClanUnit.cs

---
+++ GL-Server/Packets/Commands/Server/DonateClanUnit.cs
@@ -0,0 +1,17 @@
+﻿namespace GL.Servers.CoC.Packets.Commands.Server
+{
+    using GL.Servers.CoC.Logic;
+
+    internal class DonateClanUnit : Command
+    {
+        public DonateClanUnit(Device Device) : base(Device)
+        {
+            this.Identifier = 4;
+        }
+
+        internal override void Encode()
+        {
+
+        }
+    }
+}
