1f82368332cdb15bae6218a201dcfd7bb97a974a
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:42:24 +0300

    Create LeaveClan.cs

---
+++ GL-Server/Packets/Commands/Server/LeaveClan.cs
@@ -0,0 +1,17 @@
+﻿using GL.Servers.CoC.Logic;
+
+namespace GL.Servers.CoC.Packets.Commands.Server
+{
+    internal class LeaveClan : Command
+    {
+        public LeaveClan(Device Device) : base(Device)
+        {
+            this.Identifier = 2;
+        }
+
+        internal override void Encode()
+        {
+            
+        }
+    }
+}
