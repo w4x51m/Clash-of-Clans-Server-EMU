e70cb611baefea012837ba130a29df4c029429f1
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:38:09 +0300

    Create ClanSettingsChanged.cs

---
+++ GL-Server/Packets/Commands/Server/ClanSettingsChanged.cs
@@ -0,0 +1,17 @@
+﻿namespace GL.Servers.CoC.Packets.Commands.Server
+{
+    using GL.Servers.CoC.Logic;
+
+    internal class ClanSettingsChanged : Command
+    {
+        public ClanSettingsChanged(Device Device) : base(Device)
+        {
+            this.Identifier = 6;
+        }
+
+        internal override void Encode()
+        {
+
+        }
+    }
+}
