8d117d723a502c5aad8bb66e51445fd4797a4934
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 14:29:03 +0300

    Add files via upload

---
+++ GL-Server/Packets/Commands/Client/UpgradeGameObject.cs
@@ -0,0 +1,46 @@
+﻿using GL.Servers.CoC.Core.Network;
+using GL.Servers.CoC.Logic.Components;
+using GL.Servers.CoC.Packets.Messages.Server;
+
+namespace GL.Servers.CoC.Packets.Commands.Client
+{
+    using GL.Servers.CoC.Logic;
+    using GL.Servers.Extensions.Binary;
+
+    internal class UpgradeGameObject : Command
+    {
+        internal int ID;
+        internal bool AltResource;
+
+        public UpgradeGameObject(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            this.ID          = this.Reader.ReadInt32();
+            this.AltResource = this.Reader.ReadBoolean();
+            this.Tick        = this.Reader.ReadInt32();
+        }
+
+        internal override void Process()
+        {
+            if (this.ID >= 500000000)
+            {
+                if (this.ID >= 504000000)
+                {
+                    if (this.ID >= 506000000)
+                    {
+                        //new Out_Of_Sync(this.Device).Send();
+                    }
+                }
+                else if (!this.Device.Player.Objects.Buildings[this.ID].Construct(this.AltResource))
+                {
+                    new Out_Of_Sync(this.Device).Send();
+                }
+            }
+            else new Out_Of_Sync(this.Device).Send();
+        }
+    }
+}
