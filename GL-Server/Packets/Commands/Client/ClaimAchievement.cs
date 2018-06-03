ea595fc253cde0c51dae59858b38a5089959522f
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-03 11:45:28 +0300

    Create ClaimAchievement.cs

---
+++ GL-Server/Packets/Commands/Client/ClaimAchievement.cs
@@ -0,0 +1,34 @@
+﻿using GL.Servers.CoC.Core.Network;
+using GL.Servers.CoC.Packets.Messages.Server;
+
+namespace GL.Servers.CoC.Packets.Commands.Client
+{
+    using GL.Servers.CoC.Logic;
+    using GL.Servers.Extensions.Binary;
+
+    internal class ClaimAchievement : Command
+    {
+        internal int ID;
+
+        public ClaimAchievement(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            this.ID   = this.Reader.ReadInt32();
+            this.Tick = this.Reader.ReadInt32();
+        }
+
+        internal override void Process()
+        {
+            if (!this.Device.Player.Progress.Claim(this.ID))
+            {
+                //this.Device.Player.Achievements.Add(this.ID);
+
+                new Out_Of_Sync(this.Device).Send();
+            }
+        }
+    }
+}
