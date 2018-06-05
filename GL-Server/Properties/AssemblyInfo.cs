242a79d048965156fb227844a8e73a2146009548
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-05 19:01:17 +0300

    Add files via upload

---
+++ GL-Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GL - Clash Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GobelinLand")]
+[assembly: AssemblyProduct("GL - Clash Server")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0dcf0ce9-215b-4287-bba4-9cdf7b7ccb66")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
