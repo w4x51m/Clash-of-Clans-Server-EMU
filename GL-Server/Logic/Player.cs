0a7ba3a24b11bf68b1cf9869f092b6669268b849
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-06 17:15:30 +0300

    Update Player.cs

--- GL-Server/Logic/Player.cs
+++ GL-Server/Logic/Player.cs
@@ -38,11 +38,11 @@ namespace GL.Servers.CoC.Logic
 
         // Avatar
 
-        [JsonProperty("name")]              internal string Name            = "Bajono";
+        [JsonProperty("name")]              internal string Name            = "Darkness";
         [JsonProperty("region")]            internal string Region;
 
-        [JsonProperty("lvl")]               internal int Level              = 1;
-        [JsonProperty("xp")]                internal int Experience;
+        [JsonProperty("lvl")]               internal int Level              = 21;
+        [JsonProperty("xp")]                internal int Experience;              = 500;
 
         [JsonProperty("league_type")]       internal int League;
         [JsonProperty("score")]             internal int Score;
@@ -59,11 +59,11 @@ namespace GL.Servers.CoC.Logic
         [JsonProperty("o_season_rank")]     internal int OldSeasonRank;
         [JsonProperty("o_season_score")]    internal int OldSeasonScore;
 
-        [JsonProperty("wins")]              internal int Wins;
+        [JsonProperty("wins")]              internal int Wins;              = 100;
         [JsonProperty("loses")]             internal int Loses;
-        [JsonProperty("t_wins")]            internal int TotalWins;
+        [JsonProperty("t_wins")]            internal int TotalWins;              = 20;
         [JsonProperty("defense_wins")]      internal int DefenseWins;
-        [JsonProperty("t_defense_wins")]    internal int TotalDefenseWins;
+        [JsonProperty("t_defense_wins")]    internal int TotalDefenseWins;              = 1;
         [JsonProperty("defense_loses")]     internal int DefenseLoses;
         [JsonProperty("troop_received")]    internal int TroopReceived;
         [JsonProperty("troop_sended")]      internal int TroopSended;
