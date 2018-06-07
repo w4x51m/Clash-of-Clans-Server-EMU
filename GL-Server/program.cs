817c67d3ceb9c55987a4fc1af519754da426b254
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-07 14:08:56 +0300

    Update program.cs

--- GL-Server/program.cs
+++ GL-Server/program.cs
@@ -21,22 +21,22 @@ namespace GL.Servers.CoC
             Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
 
             Servers.Core.Consoles.Colorful.Console.WriteWithGradient(@"
-KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO             GGGGGGGGGGGGGLLLLLLLLLLL            UUUUUUUU     UUUUUUUU
-K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO        GGG::::::::::::GL:::::::::L            U::::::U     U::::::U
-K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO    GG:::::::::::::::GL:::::::::L            U::::::U     U::::::U
-K:::::::K   K::::::KO:::::::OOO:::::::OZ:::ZZZZZZZZ:::::Z O:::::::OOO:::::::O  G:::::GGGGGGGG::::GLL:::::::LL            UU:::::U     U:::::UU
-KK::::::K  K:::::KKKO::::::O   O::::::OZZZZZ     Z:::::Z  O::::::O   O::::::O G:::::G       GGGGGG  L:::::L               U:::::U     U:::::U 
-  K:::::K K:::::K   O:::::O     O:::::O        Z:::::Z    O:::::O     O:::::OG:::::G                L:::::L               U:::::D     D:::::U 
-  K::::::K:::::K    O:::::O     O:::::O       Z:::::Z     O:::::O     O:::::OG:::::G                L:::::L               U:::::D     D:::::U 
-  K:::::::::::K     O:::::O     O:::::O      Z:::::Z      O:::::O     O:::::OG:::::G    GGGGGGGGGG  L:::::L               U:::::D     D:::::U 
-  K:::::::::::K     O:::::O     O:::::O     Z:::::Z       O:::::O     O:::::OG:::::G    G::::::::G  L:::::L               U:::::D     D:::::U 
-  K::::::K:::::K    O:::::O     O:::::O    Z:::::Z        O:::::O     O:::::OG:::::G    GGGGG::::G  L:::::L               U:::::D     D:::::U 
-  K:::::K K:::::K   O:::::O     O:::::O   Z:::::Z         O:::::O     O:::::OG:::::G        G::::G  L:::::L               U:::::D     D:::::U 
-KK::::::K  K:::::KKKO::::::O   O::::::OZZZ:::::Z     ZZZZZO::::::O   O::::::O G:::::G       G::::G  L:::::L         LLLLLLU::::::U   U::::::U 
-K:::::::K   K::::::KO:::::::OOO:::::::OZ::::::ZZZZZZZZ:::ZO:::::::OOO:::::::O  G:::::GGGGGGGG::::GLL:::::::LLLLLLLLL:::::LU:::::::UUU:::::::U 
-K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO    GG:::::::::::::::GL::::::::::::::::::::::L UU:::::::::::::UU  
-K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO        GGG::::::GGG:::GL::::::::::::::::::::::L   UU:::::::::UU    
-KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO             GGGGGG   GGGGLLLLLLLLLLLLLLLLLLLLLLLL     UUUUUUUUU      
+            KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO             GGGGGGGGGGGGGLLLLLLLLLLL            UUUUUUUU     UUUUUUUU
+            K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO        GGG::::::::::::GL:::::::::L            U::::::U     U::::::U
+            K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO    GG:::::::::::::::GL:::::::::L            U::::::U     U::::::U
+            K:::::::K   K::::::KO:::::::OOO:::::::OZ:::ZZZZZZZZ:::::Z O:::::::OOO:::::::O  G:::::GGGGGGGG::::GLL:::::::LL            UU:::::U     U:::::UU
+            KK::::::K  K:::::KKKO::::::O   O::::::OZZZZZ     Z:::::Z  O::::::O   O::::::O G:::::G       GGGGGG  L:::::L               U:::::U     U:::::U 
+              K:::::K K:::::K   O:::::O     O:::::O        Z:::::Z    O:::::O     O:::::OG:::::G                L:::::L               U:::::D     D:::::U 
+              K::::::K:::::K    O:::::O     O:::::O       Z:::::Z     O:::::O     O:::::OG:::::G                L:::::L               U:::::D     D:::::U 
+              K:::::::::::K     O:::::O     O:::::O      Z:::::Z      O:::::O     O:::::OG:::::G    GGGGGGGGGG  L:::::L               U:::::D     D:::::U 
+              K:::::::::::K     O:::::O     O:::::O     Z:::::Z       O:::::O     O:::::OG:::::G    G::::::::G  L:::::L               U:::::D     D:::::U 
+              K::::::K:::::K    O:::::O     O:::::O    Z:::::Z        O:::::O     O:::::OG:::::G    GGGGG::::G  L:::::L               U:::::D     D:::::U 
+              K:::::K K:::::K   O:::::O     O:::::O   Z:::::Z         O:::::O     O:::::OG:::::G        G::::G  L:::::L               U:::::D     D:::::U 
+            KK::::::K  K:::::KKKO::::::O   O::::::OZZZ:::::Z     ZZZZZO::::::O   O::::::O G:::::G       G::::G  L:::::L         LLLLLLU::::::U   U::::::U 
+            K:::::::K   K::::::KO:::::::OOO:::::::OZ::::::ZZZZZZZZ:::ZO:::::::OOO:::::::O  G:::::GGGGGGGG::::GLL:::::::LLLLLLLLL:::::LU:::::::UUU:::::::U 
+            K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO    GG:::::::::::::::GL::::::::::::::::::::::L UU:::::::::::::UU  
+            K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO        GGG::::::GGG:::GL::::::::::::::::::::::L   UU:::::::::UU    
+            KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO             GGGGGG   GGGGLLLLLLLLLLLLLLLLLLLLLLLL     UUUUUUUUU      
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
                          V10.134.6.Cyber Daekness
             ", Color.Green, Color.Blue, 14);
