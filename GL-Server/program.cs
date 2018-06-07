23ed79c49948193cca652bab2ebf4a53a2973f7e
DARK_GEMS <37537996+Enjoyop@users.noreply.github.com>
2018-06-07 14:04:06 +0300

    Update program.cs

--- GL-Server/program.cs
+++ GL-Server/program.cs
@@ -21,27 +21,23 @@ namespace GL.Servers.CoC
             Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
 
             Servers.Core.Consoles.Colorful.Console.WriteWithGradient(@"
- 
-                                                                                                                         
-                                                                                                                         
-KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO     LLLLLLLLLLL            UUUUUUUU     UUUUUUUU
-K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO   L:::::::::L            U::::::U     U::::::U
-K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO L:::::::::L            U::::::U     U::::::U
-K:::::::K   K::::::KO:::::::OOO:::::::OZ:::ZZZZZZZZ:::::Z O:::::::OOO:::::::OLL:::::::LL            UU:::::U     U:::::UU
-KK::::::K  K:::::KKKO::::::O   O::::::OZZZZZ     Z:::::Z  O::::::O   O::::::O  L:::::L               U:::::U     U:::::U 
-  K:::::K K:::::K   O:::::O     O:::::O        Z:::::Z    O:::::O     O:::::O  L:::::L               U:::::D     D:::::U 
-  K::::::K:::::K    O:::::O     O:::::O       Z:::::Z     O:::::O     O:::::O  L:::::L               U:::::D     D:::::U 
-  K:::::::::::K     O:::::O     O:::::O      Z:::::Z      O:::::O     O:::::O  L:::::L               U:::::D     D:::::U 
-  K:::::::::::K     O:::::O     O:::::O     Z:::::Z       O:::::O     O:::::O  L:::::L               U:::::D     D:::::U 
-  K::::::K:::::K    O:::::O     O:::::O    Z:::::Z        O:::::O     O:::::O  L:::::L               U:::::D     D:::::U 
-  K:::::K K:::::K   O:::::O     O:::::O   Z:::::Z         O:::::O     O:::::O  L:::::L               U:::::D     D:::::U 
-KK::::::K  K:::::KKKO::::::O   O::::::OZZZ:::::Z     ZZZZZO::::::O   O::::::O  L:::::L         LLLLLLU::::::U   U::::::U 
-K:::::::K   K::::::KO:::::::OOO:::::::OZ::::::ZZZZZZZZ:::ZO:::::::OOO:::::::OLL:::::::LLLLLLLLL:::::LU:::::::UUU:::::::U 
-K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO L::::::::::::::::::::::L UU:::::::::::::UU  
-K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO   L::::::::::::::::::::::L   UU:::::::::UU    
-KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO     LLLLLLLLLLLLLLLLLLLLLLLL     UUUUUUUUU      
-                                                                                                                         
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO             GGGGGGGGGGGGGLLLLLLLLLLL            UUUUUUUU     UUUUUUUU
+K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO        GGG::::::::::::GL:::::::::L            U::::::U     U::::::U
+K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO    GG:::::::::::::::GL:::::::::L            U::::::U     U::::::U
+K:::::::K   K::::::KO:::::::OOO:::::::OZ:::ZZZZZZZZ:::::Z O:::::::OOO:::::::O  G:::::GGGGGGGG::::GLL:::::::LL            UU:::::U     U:::::UU
+KK::::::K  K:::::KKKO::::::O   O::::::OZZZZZ     Z:::::Z  O::::::O   O::::::O G:::::G       GGGGGG  L:::::L               U:::::U     U:::::U 
+  K:::::K K:::::K   O:::::O     O:::::O        Z:::::Z    O:::::O     O:::::OG:::::G                L:::::L               U:::::D     D:::::U 
+  K::::::K:::::K    O:::::O     O:::::O       Z:::::Z     O:::::O     O:::::OG:::::G                L:::::L               U:::::D     D:::::U 
+  K:::::::::::K     O:::::O     O:::::O      Z:::::Z      O:::::O     O:::::OG:::::G    GGGGGGGGGG  L:::::L               U:::::D     D:::::U 
+  K:::::::::::K     O:::::O     O:::::O     Z:::::Z       O:::::O     O:::::OG:::::G    G::::::::G  L:::::L               U:::::D     D:::::U 
+  K::::::K:::::K    O:::::O     O:::::O    Z:::::Z        O:::::O     O:::::OG:::::G    GGGGG::::G  L:::::L               U:::::D     D:::::U 
+  K:::::K K:::::K   O:::::O     O:::::O   Z:::::Z         O:::::O     O:::::OG:::::G        G::::G  L:::::L               U:::::D     D:::::U 
+KK::::::K  K:::::KKKO::::::O   O::::::OZZZ:::::Z     ZZZZZO::::::O   O::::::O G:::::G       G::::G  L:::::L         LLLLLLU::::::U   U::::::U 
+K:::::::K   K::::::KO:::::::OOO:::::::OZ::::::ZZZZZZZZ:::ZO:::::::OOO:::::::O  G:::::GGGGGGGG::::GLL:::::::LLLLLLLLL:::::LU:::::::UUU:::::::U 
+K:::::::K    K:::::K OO:::::::::::::OO Z:::::::::::::::::Z OO:::::::::::::OO    GG:::::::::::::::GL::::::::::::::::::::::L UU:::::::::::::UU  
+K:::::::K    K:::::K   OO:::::::::OO   Z:::::::::::::::::Z   OO:::::::::OO        GGG::::::GGG:::GL::::::::::::::::::::::L   UU:::::::::UU    
+KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO             GGGGGG   GGGGLLLLLLLLLLLLLLLLLLLLLLLL     UUUUUUUUU      
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
                          V10.134.6.Cyber Daekness
             ", Color.Green, Color.Fuchsia, 14);
 
@@ -51,7 +47,7 @@ KKKKKKKKK    KKKKKKK     OOOOOOOOO     ZZZZZZZZZZZZZZZZZZZ     OOOOOOOOO     LLL
 
             Console.WriteLine("GemsLand program is under the 'Proprietary' license.");
             Console.WriteLine("GemsLand is NOT affiliated to 'Supercell Oy'.");
-            Console.WriteLine("ClashLand does NOT own 'Clash of Clans'");
+            Console.WriteLine("GemsLand does NOT own 'Clash of Clans'");
             Console.WriteLine("Mod1 Super Exp gain");
             Console.WriteLine("Mod2 Auto Upgrade: troops, spells, heroes".);
             Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Name + " is now starting..." + Environment.NewLine);
